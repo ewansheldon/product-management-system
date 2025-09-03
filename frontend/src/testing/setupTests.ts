7b62aca2a99d6f4ef73b84efd7b401771f33305f
Ewan Sheldon <ewansheldon@gmail.com>
2025-09-03 17:24:19 +0200

    mock window.alert

--- frontend/src/testing/setupTests.ts
+++ frontend/src/testing/setupTests.ts
@@ -2,6 +2,7 @@ import '@testing-library/jest-dom';
 jest.mock("../config/api", () => ({
   API_BASE_URL: "http://foo-api.bar",
 }));
+window.alert = jest.fn();
 beforeEach(() => {
   jest.clearAllMocks();
 });
