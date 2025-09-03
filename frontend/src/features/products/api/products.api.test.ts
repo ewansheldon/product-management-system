a62059e78529d33272d14f86414cbd056a89fe51
Ewan Sheldon <ewansheldon@gmail.com>
2025-09-03 15:28:44 +0200

    test products api create product

--- frontend/src/features/products/api/products.api.test.ts
+++ frontend/src/features/products/api/products.api.test.ts
@@ -1,14 +1,28 @@
 import { API_BASE_URL } from '../../../config/api';
 import * as fetchWrapper from '../../../lib/fetchWrapper';
-import { exampleProducts } from '../../../testing/fixtures/exampleData';
+import { exampleProduct3, exampleProducts } from '../../../testing/fixtures/exampleData';
 import * as productsAPI from './products.api';
 jest.mock('../../../lib/fetchWrapper');
 const mockedFetchWrapper = fetchWrapper as jest.Mocked<typeof fetchWrapper>;
 
 describe('getProducts', () => {
-  it('gets the products with the fetch wrapper', async () => {
+  it('gets the products', async () => {
     mockedFetchWrapper.fetchJson.mockResolvedValue(exampleProducts);
     expect(await productsAPI.getProducts()).toBe(exampleProducts);
-    expect(mockedFetchWrapper.fetchJson).toHaveBeenCalledWith(`${API_BASE_URL}/products`);
+  });
+});
+
+describe('createProduct', () => {
+  it('creates the product', async () => {
+    mockedFetchWrapper.fetchJson.mockResolvedValue(exampleProduct3);
+    const formData = new FormData();
+    formData.append('name', exampleProduct3.name);
+    formData.append('artist', exampleProduct3.artist);
+    const testCoverArt = new File(['coverArt'], 'coverArt.jpg', { type: 'image/jpg' });
+    formData.append('coverArt', testCoverArt);
+    expect(await productsAPI.createProduct(formData)).toBe(exampleProduct3);
+
+    const reqOpts = { method: 'POST', body: formData };
+    expect(mockedFetchWrapper.fetchJson).toHaveBeenCalledWith(`${API_BASE_URL}/products`, reqOpts);
   });
 });
